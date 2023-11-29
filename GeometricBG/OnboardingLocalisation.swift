51cf7ee2a17d80c75d8e9581e08d5aaee435fcae
Samuel Corke <samuelcorke@protonmail.com>
2023-11-29 22:23:35 +0000

    hat tip : Mendy Barouk

--- GeometricBG/OnboardingLocalisation.swift
+++ GeometricBG/OnboardingLocalisation.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 // CONVERT LOCALISED TEXT WITH SAFETY OF ENUM !
+// h/t: Mendy Barouk : https://medium.com/@mendibarouk/enhance-your-localized-capabilities-on-your-ios-applications-d3ba17138077
 enum OnboardingLocalisation: String {
     
     case onboarding1, onboarding2, onboarding3, onboarding4, onboarding5a, onboarding5b, onboarding6a, onboarding6b, onboarding7
